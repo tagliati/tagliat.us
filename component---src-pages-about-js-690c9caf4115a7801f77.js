119d03c629402cd51b7090937b76c6477187811b
André Tagliati <tagliati@gmail.com>
2023-09-23 22:19:57 +0000

    next step

--- component---src-pages-about-js-690c9caf4115a7801f77.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{212:function(e,t,n){"use strict";n.r(t);var a=n(0),l=n.n(a),u=n(209);t.default=function(){return l.a.createElement(u.a,null,l.a.createElement("h1",null,"About the Author"),l.a.createElement("p",null,"Welcome to my Gatsby site."))}}}]);
-//# sourceMappingURL=component---src-pages-about-js-690c9caf4115a7801f77.js.map
