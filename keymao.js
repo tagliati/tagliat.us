169c6cd9c6b9d39329d007b2216748456fd0b15f
André Tagliati <tagliati@gmail.com>
2023-11-14 06:39:56 -0300

    Update keymao.js

--- keymao.js
+++ keymao.js
@@ -11,9 +11,9 @@ document.addEventListener(
     if (event.ctrlKey) {
       // Even though event.key is not 'Control' (e.g., 'a' is pressed),
       // event.ctrlKey may be true if Ctrl key is pressed at the same time.
-      alert(`Combination of ctrlKey + ${keyName}`);
+       document.getElementById("key").innerHTML = `Combination of ctrlKey + ${keyName}`;
     } else {
-      alert(`Key pressed ${keyName}`);
+      document.getElementById("key").innerHTML = `Key pressed ${keyName}`;
     }
   },
   false,
@@ -27,7 +27,7 @@ document.addEventListener(
     // As the user releases the Ctrl key, the key is no longer active,
     // so event.ctrlKey is false.
     if (keyName === "Control") {
-      alert("Control key was released");
+      document.getElementById("key").innerHTML = "Control key was released";
     }
   },
   false,
