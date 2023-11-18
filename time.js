810e2f04aba724d69b6b972d851d467e7786d756
André Tagliati <andre.tagliati@mercadolivre.com>
2023-11-18 08:49:33 -0300

    dddd

--- time.js
+++ time.js
@@ -1,8 +1,8 @@
 var myVar = setInterval(function() {
   myTimer();
-}, 1000);
-
+}, 60000);
+const zeroPad = (num, places) => String(num).padStart(places, '0')
 function myTimer() {
   var d = new Date();
-  document.getElementById("clock").innerHTML = d.toLocaleString('pt-BR', { timeZone: 'America/Sao_Paulo' });
+  document.getElementById("clock").innerHTML = `<span class="date">${zeroPad(d.getDate(),2)}/${zeroPad(d.getMonth()+1,2)}/${d.getFullYear()}</span><span class="hour">${zeroPad(d.getHours(),2)}:${zeroPad(d.getMinutes(),2)}</span>`;
 }
