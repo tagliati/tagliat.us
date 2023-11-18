853dad8ee2e6087bbd04522152b2b61866c39eac
André Tagliati <andre.tagliati@mercadolivre.com>
2023-11-18 09:01:45 -0300

    elucubracoes

--- time.js
+++ time.js
@@ -6,4 +6,6 @@ function myTimer() {
   var d = new Date();
   document.getElementById("clock").innerHTML = `<span class="date">${zeroPad(d.getDate(),2)}/${zeroPad(d.getMonth()+1,2)}/${d.getFullYear()}</span><span class="hour">${zeroPad(d.getHours(),2)}:${zeroPad(d.getMinutes(),2)}</span>`;
 }
-myTimer();
+window.onload = function(){
+  myTimer();
+}
