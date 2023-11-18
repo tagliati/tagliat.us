9a2ae826915d929dd89674a22d9525b4ec400a47
André Tagliati <andre.tagliati@mercadolivre.com>
2023-11-18 08:53:12 -0300

    x

--- time.js
+++ time.js
@@ -1,4 +1,4 @@
-var myVar = setInterval(function() {
+var timeCop = setInterval(function() {
   myTimer();
 }, 60000);
 const zeroPad = (num, places) => String(num).padStart(places, '0')
@@ -6,3 +6,4 @@ function myTimer() {
   var d = new Date();
   document.getElementById("clock").innerHTML = `<span class="date">${zeroPad(d.getDate(),2)}/${zeroPad(d.getMonth()+1,2)}/${d.getFullYear()}</span><span class="hour">${zeroPad(d.getHours(),2)}:${zeroPad(d.getMinutes(),2)}</span>`;
 }
+timeCop();
